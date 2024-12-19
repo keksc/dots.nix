8691295f747ddd2395bd85f5e418e7cfe96039db
kekw <pboursin2008@orange.fr>
2024-12-19 18:58:13 +0100

    a

--- flake.nix
+++ flake.nix
@@ -28,6 +28,7 @@
       home-manager,
       lanzaboote,
       zen-browser,
+      nixos,
       ...
     }:
     let
@@ -37,12 +38,42 @@
     in
     {
       nixosConfigurations = {
+        iso = lib.nixosSystem {
+          modules = [
+            "${nixos}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+            ./configuration.nix
+            lanzaboote.nixosModules.lanzaboote
+            (
+              { pkgs, lib, ... }:
+              {
+
+                environment.systemPackages = [
+                  # For debugging and troubleshooting Secure Boot.
+                  pkgs.sbctl
+                  zen-browser.packages.${pkgs.system}.default
+                ];
+
+                # Lanzaboote currently replaces the systemd-boot module.
+                # This setting is usually set to true in configuration.nix
+                # generated at installation time. So we force it to false
+                # for now.
+                boot.loader.systemd-boot.enable = lib.mkForce false;
+
+                boot.lanzaboote = {
+                  enable = true;
+                  pkiBundle = "/etc/secureboot";
+                };
+              }
+            )
+
+          ];
+        };
+
         nixos = lib.nixosSystem {
           inherit system;
           modules = [
             ./configuration.nix
             lanzaboote.nixosModules.lanzaboote
-
             (
               { pkgs, lib, ... }:
               {
