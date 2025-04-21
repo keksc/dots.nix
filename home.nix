7f8b887527a9dc45a1a94f07809e3bdfce54ded2
kekw <pboursin2008@orange.fr>
2025-04-21 15:55:29 +0200

    a

--- home.nix
+++ home.nix
@@ -31,6 +31,7 @@
     ardour
     vital
     blender
+    unityhub
 
     vesktop
     brightnessctl
@@ -177,7 +178,32 @@
       };
     };
   };
-  services.ollama.enable = true;
+  home.pointerCursor = {
+    gtk.enable = true;
+    # x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 16;
+  };
+
+  gtk = {
+    enable = true;
+
+    theme = {
+      package = pkgs.flat-remix-gtk;
+      name = "Flat-Remix-GTK-Grey-Darkest";
+    };
+
+    iconTheme = {
+      package = pkgs.adwaita-icon-theme;
+      name = "Adwaita";
+    };
+
+    font = {
+      name = "Sans";
+      size = 11;
+    };
+  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
